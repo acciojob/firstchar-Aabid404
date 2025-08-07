71b7fce024e62de77705c9fde0132616569208e5
AABID DEVELOPER <150765769+Aabid404@users.noreply.github.com>
2025-08-07 13:45:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 function firstChar(text) {
-  // your code here
+  // Trim leading and trailing spaces, then check first character
+  for (let i = 0; i < text.length; i++) {
+    if (text[i] !== ' ') {
+      return text[i];
+    }
+  }
+  return ''; // Return empty string if no non-space character is found
 }
 
 // Do not change the code below
-//Uncomment the following line to show the prompt popup
-//const text = prompt("Enter text:");
+// Uncomment the following line to show the prompt popup
+const text = prompt("Enter text:");
 alert(firstChar(text));
